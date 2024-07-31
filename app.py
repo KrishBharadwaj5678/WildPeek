5cda8fc04fae9769860635422a4c315cbedc06f1
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-07-31 16:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     }
 )
 
-st.write("<h2 style='color:#8cc730;font-size:35px;'>Your Quick Guide to Animal Information</h2>",unsafe_allow_html=True)
+st.write("<h2 style='color:#8cc730;font-size:34px;'>Your Quick Guide to Animal Information</h2>",unsafe_allow_html=True)
 
 name=st.text_input("Enter Animal Name",placeholder="king cobra")
 
@@ -20,36 +20,38 @@ if btn:
     response = requests.get(api_url, headers={'X-Api-Key': '2jWCY0dASiPZc7RLybXvXA==R9oC0XPKPWiGJ6k6'})
     if response.status_code == requests.codes.ok:
         data=response.json()
-        for i in data:
-            animalName=i['name']
+        if(len(data)>=1):
+            for i in data:
+                animalName=i['name']
+                # Extracting Locations
+                locations=''
+                for j in i['locations']:
+                    locations+=j+', '
 
-            # Extracting Locations
-            locations=''
-            for j in i['locations']:
-                locations+=j+', '
+                st.write(f"<h2 style='color:#FFDE4D;font-size:34px;'>{animalName}</h2>",unsafe_allow_html=True)
 
-            st.write(f"<h2 style='color:#FFDE4D;font-size:34px;'>{animalName}</h2>",unsafe_allow_html=True)
+                def showFeatures(label,data):
+                    st.write(f"<li style='font-size:25.3px;'>{label}: {data}</li>",unsafe_allow_html=True)
 
-            def showFeatures(label,data):
-                st.write(f"<li style='font-size:25.3px;'>{label}: {data}</li>",unsafe_allow_html=True)
+                showFeatures('Locations',locations.rstrip(", "))
 
-            showFeatures('Locations',locations.rstrip(", "))
+                # Extracting Characteristics
+                characteristics_keys=i['characteristics'].keys()
+                for key in characteristics_keys:
 
-            # Extracting Characteristics
-            characteristics_keys=i['characteristics'].keys()
-            for key in characteristics_keys:
+                    modified_key=' '.join(i.title() for i in key.split("_"))
 
-                modified_key=' '.join(i.title() for i in key.split("_"))
-
-                if(key=='color'):
-                    colors=', '.join(re.findall(r'[A-Z][a-z]*',i['characteristics'][key]))
-                    showFeatures(modified_key,colors)
-                elif(key=='other_name(s)'):
-                    showFeatures('Other Names',i['characteristics'][key])
-                else:
-                    showFeatures(modified_key,i['characteristics'][key])
-            
-            # Extracting Taxonomy
-            taxonomy_keys=i['taxonomy'].keys()
-            for key in taxonomy_keys:
-                    showFeatures(key.title(),i['taxonomy'][key])
+                    if(key=='color'):
+                        colors=', '.join(re.findall(r'[A-Z][a-z]*',i['characteristics'][key]))
+                        showFeatures(modified_key,colors)
+                    elif(key=='other_name(s)'):
+                        showFeatures('Other Names',i['characteristics'][key])
+                    else:
+                        showFeatures(modified_key,i['characteristics'][key])
+                
+                # Extracting Taxonomy
+                taxonomy_keys=i['taxonomy'].keys()
+                for key in taxonomy_keys:
+                        showFeatures(key.title(),i['taxonomy'][key])
+        else:
+            st.warning("Oops! No Data for This Animal")
